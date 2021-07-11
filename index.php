ca2723b69b829314788d55dcd6d70c7358682439
Andchad <52137172+Andchad@users.noreply.github.com>
2021-07-11 10:39:08 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php header('Location: /home.html'); ?>
+<?php 
+
+include_once("home.html"); 
+include_once("about.html"); 
+include_once("contact.html"); 
+include_once("services.html"); 
+
+include("/assets"); 
+include("/demo"); 
+include("/vendor"); 
+
+?>
